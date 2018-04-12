11104b8208ed0996d4f832064e3d5dfa80d05460
Scott McCaughey <scott@scottmccaughey.com>
2018-04-12 10:01:45 -0400

    Adding closed issues switch

--- gh-pages/src/app/modules/list/issue/issue.component.ts
+++ gh-pages/src/app/modules/list/issue/issue.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../data/api.service';
 import { UiSwitchModule } from 'angular2-ui-switch';
