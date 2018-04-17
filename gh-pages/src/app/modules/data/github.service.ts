159a1ca824e889834dc41f2833d8c7979dedbb80
Scott McCaughey <scott@scottmccaughey.com>
2018-04-17 15:38:39 -0400

    Obfuscating API token

--- gh-pages/src/app/modules/data/github.service.ts
+++ gh-pages/src/app/modules/data/github.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable()
 export class GithubService {
@@ -9,27 +9,35 @@ export class GithubService {
   private githubRoot = 'https://api.github.com/repos/ombegov/ITDB-schema/';
   private githubContents = 'contents/src/';
   private githubCommits = 'commits?path=';
-  private token = 'f255300ec05626a44e86d65f755b1567f5337c4f';
+  private tokenCode = 'U2FsdGVkX19tdJtCz389lUqX76HeLWh6I9Sl5FheRAHsm8lnIRXPe7rO0JpBiwXOqjoTConymgkOCYRRogf1+A==';
+  private salt = 'sHjvqU7bNyKZw0ArXwCB';
 
   constructor( private _http: HttpClient ) { }
 
+  getToken() {
+    return CryptoJS.AES.decrypt(this.tokenCode, this.salt).toString(CryptoJS.enc.Utf8);
+  }
+
   getSchemaCats() {
+    const token = this.getToken();
     const headers = new HttpHeaders();
     return this._http.get(this.githubRoot + this.githubContents, {
-      headers: new HttpHeaders({'Authorization': 'token ' + this.token})
+      headers: new HttpHeaders({'Authorization': 'token ' + token})
     });
   }
 
   getExamples(schemaCat) {
+    const token = this.getToken();
     const exampleFormat = (schemaCat === 'InvestmentReport') ? '/examples' : '/Examples';
     return this._http.get(this.githubRoot + this.githubContents + schemaCat + exampleFormat, {
-      headers: new HttpHeaders({'Authorization': 'token ' + this.token})
+      headers: new HttpHeaders({'Authorization': 'token ' + token})
     });
   }
 
   getFileInfo(path) {
+    const token = this.getToken();
     return this._http.get(this.githubRoot + this.githubCommits + path, {
-      headers: new HttpHeaders({'Authorization': 'token ' + this.token})
+      headers: new HttpHeaders({'Authorization': 'token ' + token})
     });
   }
 
